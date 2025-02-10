6341abc1a0f1f57caa2495395f7ac46abcdc89ec
Antoine Caron-Guay <antoine.caron-guay@umontreal.ca>
2025-02-10 08:45:47 -0500

    new args

--- python/metashape_workflow_lefolab.py
+++ python/metashape_workflow_lefolab.py
@@ -11,9 +11,8 @@ import sys
 
 # ---- If this is a first run from the standalone python module, need to copy the license file from the full metashape install: from python import metashape_license_setup
 
-## Define where to get the config file (only used if running interactively)
-manual_config_file = "config/example_dev.yml"
-# ---- If not running interactively, the config file should be supplied as the command-line argument after the python script, e.g.: python metashape_workflow.py config.yml
+## Define where to get the config file
+default_config_file = "config/config_lefolab_default.yml"
 
 
 # Load custom modules and config file: slightly different depending whether running interactively or via command line
@@ -24,41 +23,51 @@ except:  # running from command line (in linux) or interactively (windows)
 
 
 def parse_args():
-    parser = argparse.ArgumentParser(
-        description="The first required argument is the path to the config file. "
-        + "All other arguments are optional overrides to the corresponding entry in that config"
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-c",
+        "--config_file",
+        default=default_config_file,
+        help="A path to a yaml config file."
     )
     parser.add_argument(
-        "config_file", default=manual_config_file, help="A path to a yaml config file."
-    )
-    parser.add_argument(
-        "--photo-path",
+        "-i",
+        "--images-path",
         nargs="+",
         help="One or more absolute paths to load photos from, separated by spaces.",
     )
     parser.add_argument(
-        "--photo-path-secondary",
-        help="A path to a folder of images to add after alignment. "
-        + "For more information, see the description in the example config file.",
-    )
-    parser.add_argument(
+        "-p",
         "--project-path",
-        help="Path to save Metashape project file (.psx). Will be created if does not exist",
+        help="Path to save Metashape project file (.psx). Will be created if does not exist.",
     )
     parser.add_argument(
+        "-o",
         "--output-path",
-        help="Path for exports (e.g., points, DSM, orthomosaic) and processing log. "
+        help="Path for exports (e.g., cloudpoint, DSM, orthomosaic) "
         + "Will be created if does not exist.",
     )
     parser.add_argument(
+        "-r",
         "--run-name",
         help="The identifier for the run. Will be used in naming output files.",
     )
     parser.add_argument(
+        "-crs",
         "--project-crs",
         help="CRS EPSG code that project outputs should be in "
         + "(projection should be in meter units and intended for the project area). "
         + "It should be specified in the following format: 'EPSG::<EPSG code>'.",
+    )
+    parser.add_argument(
+        "--highdis-disabled",
+        action="store_true",
+        help="Disable High quality and No filering (HighDis) processing. Enabled by default.",
+    )
+    parser.add_argument(
+        "--keep-project",
+        action="store_true",
+        help="Keep project file after processing. Deleted by default.",
     )
 
     args = parser.parse_args()
@@ -67,9 +76,30 @@ def parse_args():
 
 args = parse_args()
 
-# Initialize the workflow instance with the configuration file and the dictionary representation of
-# CLI overrides
+
+# Check if required parameters are provided or appropriate config file is used
+required_params = {
+    "images_path": "Error: No images path provided. Please specify --images-path or use an appropriate config file.",
+    "output_path": "Error: No output path provided. Please specify --output-path or use an appropriate config file.",
+    "run_name": "Error: No run name provided. Please specify --run-name or use an appropriate config file.",
+    "project_crs": "Error: No project CRS provided. Please specify --project-crs or use an appropriate config file."
+}
+
+if args.config_file == default_config_file:
+    for param, error_message in required_params.items():
+        if getattr(args, param) is None:
+            raise ValueError(error_message)
+
+# Check if --keep-project is True and either --project-path or --config-file is missing
+if (args.keep_project and (args.config_file != default_config_file or args.project_path is not None)):
+    pass
+elif not args.keep_project:
+    pass
+else:
+    raise ValueError("Error: --keep-project is set, but no project path or config file was provided.")
+
+# Initialize the workflow instance with the configuration file and the dictionary representation of CLI overrides
 meta = MetashapeWorkflowLefolab(config_file=args.config_file, override_dict=args.__dict__)
 
-### Run the Metashape workflow
+# Run the Metashape workflow
 meta.run()
